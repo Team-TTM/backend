3db2717d95156561ba9a96260bfadc7091236cfb
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-01-29 13:14:35 +0100

    edit : ajout des routes vue

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const auth = require('../middleware/auth')
 const passport = require('passport');
 const authController = require("../controllers/authController");
+const path = require("path");
 
 
-
-// router.get('/auth',authRedirectController );
-
+    console.log('URL:', url);
+    console.log('Headers:', headers);
+    console.log('Body:', body);
+    console.log('Params:', params);
+    res.sendFile(path.join(__dirname, '..','dist', 'index.html'));
+});
 router.post("/auth/google", passport.authenticate('google-token', { session: false }),authController.googleAuthController);
 
 router.post('/auth/facebook', passport.authenticate('facebook-token', {session: false}),authController.facebookAuthController);
