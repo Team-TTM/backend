8cec2693fb8c42599ea9e22c29e077d4fbe63cee
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-02-13 15:35:50 +0100

    edit : model user

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -4,11 +4,13 @@ const createUserTable = async () => {
     const query = `
         CREATE TABLE IF NOT EXISTS users (
             id_user SERIAL PRIMARY KEY,
+            id_adherant INTEGER UNIQUE ,
             role VARCHAR(255) CHECK IN ('user','dirigent') NOT NULL DEFAULT 'user',
             charte_signe BOOLEAN NOT NULL DEFAULT FALSE,
             google_id VARCHAR(255) UNIQUE DEFAULT null,
             facebook_id VARCHAR(255) UNIQUE,
-            newsletter BOOLEAN NOT NULL DEFAULT FALSE
+            newsletter BOOLEAN NOT NULL DEFAULT FALSE,
+            FOREIGN KEY (id_adherant) REFERENCES adherant(id_adherant) ON DELETE CASCADE
             );
     `;
     try {
