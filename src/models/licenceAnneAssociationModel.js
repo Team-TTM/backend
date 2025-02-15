74243ba0911d446b798653e6b209a7d6ee0045c6
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-02-15 14:48:58 +0100

    edit : passage a une seul table adherant

--- src/models/licenceAnneAssociationModel.js
+++ src/models/licenceAnneAssociationModel.js
@@ -1,14 +1,14 @@
 const client = require('../config/database'); // Connexion à la base de données
 
 
-const   createLicenceAnneeAssociationTable = async () => {
+const  createLicenceAnneeAssociationTable = async () => {
     const query = `
         CREATE TABLE IF NOT EXISTS licence_annee_association(
             id_anne        INTEGER NOT NULL, -- Référence à l'année dans la table \`anne_licence\`
             numero_licence  VARCHAR(255) NOT NULL, -- Référence à \`numero_licence\` dans \`licences\`
             PRIMARY KEY (id_anne, numero_licence),
             FOREIGN KEY (id_anne) REFERENCES anne_licence (anne) ON DELETE CASCADE,
-            FOREIGN KEY (numero_licence) REFERENCES licences (numero_licence) ON DELETE CASCADE
+            FOREIGN KEY (numero_licence) REFERENCES adherants (numero_licence) ON DELETE CASCADE
         );
     `;
     try {
