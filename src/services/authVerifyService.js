0f7b3b08ced70aa484839a7db7ab049120ed0e29
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-02-15 21:10:26 +0100

    doc : ajout de la js doc

--- src/services/authVerifyService.js
+++ src/services/authVerifyService.js
@@ -1,5 +1,16 @@
 const userService = require("./userService");
 const {createToken} = require("./tokenService");
+
+
+/**
+ * Gère la vérification et l'authentification des utilisateurs via Google ou Facebook.
+ *
+ * @async
+ * @param {"google"|"facebook"} platform - La plateforme d'authentification (Google ou Facebook).
+ * @param {Object} profile - Le profil utilisateur retourné par la plateforme OAuth.
+ * @param {Function} done - Callback de fin d'authentification.
+ * @returns {Promise<void>} - Retourne un objet contenant le token et l'état de l'adhésion.
+ */
 const handleAuthVerification = async (platform, profile, done) => {
     try {
         const platformId = profile.id; // Identifiant spécifique à la plateforme (Google ou Facebook)
@@ -17,20 +28,32 @@ const handleAuthVerification = async (platform, profile, done) => {
                 ? await userService.createUserGoogle(platformId)
                 : await userService.createUserFacebook(platformId);
         }
-
-
-        const isAdherant = user.id_adherant != null;
+        const isAdherent = user.numero_licence != null;
 
         const token = createToken(user.id_user);
 
-        return done(null, {token, licenceExiste: isAdherant});
+        return done(null, {token, licenceExiste: isAdherent});
     } catch (error) {
         console.error(`Erreur dans ${platform} Auth:`, error);
         return done(error);
     }
 };
-
+/**
+ * Vérification d'authentification via Google.
+ *
+ * @param {string} accessToken - Le token d'accès OAuth.
+ * @param {Object} profile - Le profil utilisateur Google.
+ * @param {Function} done - Callback de fin d'authentification.
+ */
 const googleAuthVerify = (accessToken, profile, done) => handleAuthVerification('google', profile, done);
+
+/**
+ * Vérification d'authentification via Facebook.
+ *
+ * @param {string} accessToken - Le token d'accès OAuth.
+ * @param {Object} profile - Le profil utilisateur Facebook.
+ * @param {Function} done - Callback de fin d'authentification.
+ */
 const facebookAuthVerify = (accessToken, profile, done) => handleAuthVerification('facebook', profile, done);
 
 module.exports = {
