ad9b763cb455099b5ca6d3a2bd389ac7ad939935
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-02-15 21:32:42 +0100

    doc : ajout de la js doc

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,6 +3,26 @@ const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 
+/**
+ * Middleware d'authentification utilisant JWT.
+ * Vérifie la validité du token et ajoute l'ID utilisateur à `req.auth`.
+ *
+ * @param {import("express").Request} req - Objet de requête Express.
+ * @param {import("express").Response} res - Objet de réponse Express.
+ * @param {import("express").NextFunction} next - Fonction pour passer au middleware suivant.
+ *
+ * @returns {void} - Passe au middleware suivant si l'authentification réussit, sinon redirige avec une erreur.
+ *//**
+ * Middleware d'authentification utilisant JWT.
+ * Vérifie la validité du token et ajoute l'ID utilisateur à `req.auth`.
+ *
+ * @param {import("express").Request} req - Objet de requête Express.
+ * @param {import("express").Response} res - Objet de réponse Express.
+ * @param {import("express").NextFunction} next - Fonction pour passer au middleware suivant.
+ *
+ * @returns {void} - Passe au middleware suivant si l'authentification réussit, sinon redirige avec une erreur.
+ */
+
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
