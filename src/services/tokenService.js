6da1995e98ed83302d8bcc1275e08bfd493ff2ef
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-02-17 11:53:20 +0100

    doc : documentation for createToken function in tokenService.js

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -2,7 +2,12 @@ const jwt = require('jsonwebtoken');
 const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-
+/**
+ * Crée un JSON Web Token (JWT) pour un identifiant utilisateur donné.
+ *
+ * @param {string} userId - L'identifiant de l'utilisateur pour lequel le token est créé.
+ * @returns {string} - Le JWT généré.
+ */
 const createToken = (userId) => {
     return jwt.sign(
         { userId: userId},
