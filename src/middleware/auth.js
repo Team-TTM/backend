27c166092c22a758bdba6c150c564cda9e7e0ea5
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-03-16 00:07:28 +0100

    fix: rollback version

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const path = require('path');
+const {findUserByUserId} = require('../services/userService');
 require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 
 
@@ -38,4 +39,35 @@ const authenticateJWT = (req, res, next) => {
     }
 };
 
-module.exports = {authenticateJWT};
+const authenticateDirigeant = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({message: 'Token manquant ou invalide'});
+        }
+
+        const token = authHeader.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decodedToken || !decodedToken.userId) {
+            return res.status(401).json({message: 'Token invalide'});
+        }
+
+        const user = await findUserByUserId(decodedToken.userId);
+        // const role = await getUserRole(decodedToken.userId);
+        if (!user) {
+            return res.status(401).json({message: 'Utilisateur non trouvé'});
+        }
+        if(user.role !== 'dirigeant'){
+            return res.status(401).json({message: 'Vous n\'avez pas les droits pour accéder à cette ressource'});
+        }
+
+        req.auth = {userId: decodedToken.userId};
+        next();
+    } catch (error) {
+        console.error('Erreur JWT:', error.message);
+        return res.status(401).json({message: 'Authentification échouée'});
+    }
+};
+
+module.exports = {authenticateJWT, authenticateDirigeant};
---
+++ src/routes/eventRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {authenticateDirigeant} = require('../middleware/auth');
+const eventController = require('../controllers/eventController');
+
+router.get('getEvents',authenticateDirigeant,eventController.getEvents);
+router.get('getEvent/:id',authenticateDirigeant,eventController.getEvent);
+router.post('createEvent',authenticateDirigeant,eventController.createEvent);
+router.put('editEvent',authenticateDirigeant,eventController.editEvent);
+router.delete('deleteEvent/:id',authenticateDirigeant,eventController.deleteEvent);
+
+
